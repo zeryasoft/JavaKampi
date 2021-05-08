4bf19099be9f30b15a430df083b61b21ded327ea
zeryasoft <zeryasoft@gmail.com>
2021-05-09 00:33:08 +0300

    09.05.2021

---
+++ northwind/src/main/java/kodlamaio/northwind/api/controllers/ProductsController.java
@@ -0,0 +1,30 @@
+package kodlamaio.northwind.api.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kodlamaio.northwind.business.abstracts.ProductService;
+import kodlamaio.northwind.entities.concretes.Product;
+
+@RestController
+@RequestMapping("/api/products")//api/products 'ı isteyen olursa burası cevap verir
+public class ProductsController {
+	
+	@Autowired
+	private ProductService productService;
+	
+	public ProductsController(ProductService productService) {
+		super();
+		this.productService = productService;
+	}
+
+	@GetMapping("/getall")
+	public List<Product> getAll(){
+		return this.productService.getAll();
+	}
+	
+}
